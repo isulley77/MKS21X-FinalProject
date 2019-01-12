accd009d23ff47972641904241766df5f3419105
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-01-12 17:00:00 -0500

    main() function

--- Driver.java
+++ Driver.java
@@ -3,8 +3,12 @@ import java.imageio.*;
 
 public class Driver{
 
-      FractalPlane plane = new FractalPlane(500, name);
-      File f = new File(name + ".png");
-      ImageIO.write(plane, "PNG", f);
+  public static void main(String[] args){
 
+    String name = args[0];
+
+    FractalPlane plane = new FractalPlane(500, name);
+    File f = new File(name + ".png");
+    ImageIO.write(plane, "PNG", f);
+  }
 }
