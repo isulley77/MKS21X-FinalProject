cc43d691f02eb3f743d23216de7b8ec1f1276b14
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-01-12 16:59:34 -0500

    added attributes

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,3 +1,16 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
 public class Sierpinski{
-  
+
+  private int steps;
+  private Color shapeColor;
+
+  public void initialTriangle(){
+
+  }
+
+
+
 }
