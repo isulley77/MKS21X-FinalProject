e9b4a26bea2e87e74148c1eeac9bae6d8c5d38e8
isulley77 <isulley@stuy.edu>
2019-01-19 12:09:24 -0500

    fractal selection

--- FractalPlane.java
+++ FractalPlane.java
@@ -36,6 +36,9 @@ public class FractalPlane{
     if(type.equals("kochsnowflake"){
         new KochSnowflake(imagePlane, drawing, fractalColor, iterations);
         }
+    if(type.equals("sierpinski"){
+        new Sierpinski(imagePlane, drawing, fractalColor, iterations);
+        }
     //paint(drawing);
     //drawKoch(0, HEIGHT/2 ,WIDTH, HEIGHT/2, drawing, iterations);
     save(name);
