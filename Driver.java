4b76e7b2a78c854d1ff0b4534f8e8fbcea1ff7bf
isulley77 <isulley@stuy.edu>
2019-01-19 12:01:47 -0500

    Scanner questions

--- Driver.java
+++ Driver.java
@@ -23,5 +23,16 @@ public class Driver{
     //FractalPlane.setIterations(parseInt(iterations));
     
     
+    
+    // 1 Please Enter the fractal you would like to draw (KochSnowflake, Sierpinski)
+    
+    // 2 Please Enter the fractal color you would like
+    
+    // 3 Please enter the background color you would like
+    
+    // 4 Please enter the number of iterations for your fractal
+    
+    // 5 Please enter a filename for your fractal image
+    
   }
 }
