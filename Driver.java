94cca26e3e5d29f746d7aa7b1824d13ee4155fd2
isulley77 <isulley@stuy.edu>
2019-01-20 14:28:24 -0500

    added scanner input

--- Driver.java
+++ Driver.java
@@ -5,16 +5,24 @@ import java.util.Scanner;
 
 public class Driver{
 
+    String type;
+    String fileName;
+    String fractalColor;
+    String backgroundColor;
+    int iterations;
+
   public static void main(String[] args){
 
    Scanner userInput = new Scanner(System.in);
+   
+   
     
     //Color bgcolor = c.getColor(args[2]);
     //Color frcolor = c.getColor(args[3]);
 
-    if(
+    //if(
     
-    new FractalPlane(args[0], Integer.parseInt(args[1]), args[2], args[3]);
+    //new FractalPlane(args[0], Integer.parseInt(args[1]), args[2], args[3]);
     
     //String name = args[0];
     //String iterations = args[1];
@@ -26,14 +34,28 @@ public class Driver{
     
     
     // 1 Please Enter the fractal you would like to draw (KochSnowflake, Sierpinski)
+    System.out.print("Please Enter the fractal you would like to draw (KochSnowflake, Sierpinski)");
+    type = userInput.next();
     
     // 2 Please Enter the fractal color you would like
+    System.out.print("Enter the fractal color you would like:");
+    fractalColor = userInput.next();
     
     // 3 Please enter the background color you would like
+    System.out.print("Enter the background color you would like:");
+    backgroundColor = userInput.next();
+    
+    
     
     // 4 Please enter the number of iterations for your fractal
+    System.out.print("Enter the number of iterations you would like:");
+    iterations = userInput.next();
     
     // 5 Please enter a filename for your fractal image
+    System.out.print("Enter the filename for your fractal image:");
+    fileName = userInput.next();
+    
+    
     
   }
 }
