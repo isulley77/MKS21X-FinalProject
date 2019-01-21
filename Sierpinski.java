4cd4d811e3f4ebf07d965476774df499a4c29579
isulley77 <isulley@stuy.edu>
2019-01-20 21:37:34 -0500

    paint method

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,19 +1,38 @@
-//import java.awt.Color;
-//import java.awt.Graphics2D;
+import java.awt.image.*;
+import java.awt.Graphics;
+import java.lang.Math;
+import java.awt.Color;
+import java.awt.Point;
 
 
 public class Sierpinski extends FractalPlane{
 
 
     public Sierpinski(String filename, int iter, String bgcolor, String frcolor){
+        
         super(filename, iter, bgcolor, frcolor);
+        
+        setColors(bgcolor, frcolor);
+        setBackgroundColor();
+        
+        paint();
+        save(filename);
     
     
+    }
+    
+    public void paint(Graphics drawing){
+        
+        Point p1 = new Point(WIDTH / 2, 100);
+        Point p2 = new Point(100, HEIGHT - 100);
+        Point p3 = new Point(WIDTH - 100, Height - 100);
+        
+        drawSierpinkski(p1, p2, p3, drawing, iterations);
+        
+        //drawing.setColor(fractalColor);
     
     }
     
 }
 
-  //private int steps;
-  //private Color shapeColor;
 
